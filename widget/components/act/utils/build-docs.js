c2b26fe6d6f68aa32a0e4b5be47fa08bc816b885
杨永安 <i@yangyongan.com>
2021-06-05 17:23:36 +0800

    ci: 优化文档生成工具路径自适应

--- widget/components/act/utils/build-docs.js
+++ widget/components/act/utils/build-docs.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const path = require('path');
+
 const {Parser} = require('mark-to-jsonml');
 const parser = new Parser();
 
@@ -9,16 +11,23 @@ let readme = `# ACT组件文档
 
 > 文档已经迁移到各组件所在的文件夹中。\n\n`;
 
-const components = fs.readdirSync('../').filter(item => item.startsWith('a-'));
+const actRoot = path.resolve(__dirname, '../');
+
+
+const components = fs.readdirSync(actRoot).filter(item => item.startsWith('a-'));
+let total = 0;
 components.forEach(com => {
-    const md = `../${com}/readme.md`;
+    const md = `${actRoot}/${com}/readme.md`;
     try {
         const parsed = parser.parse(fs.readFileSync(md).toString());
         readme += (`## ${parsed[2][2]}  \n\n \` 组件介绍 \` ${parsed[4][1]}\n\n \` 文档链接 \` [./${com}/readme.md](./${com}/readme.md) \n\n \` 最近更新 \` ${fs.statSync(md).mtime.toLocaleString()} \n\n`);
+        total++;
     } catch (e) {
 
     }
 })
 
 
-fs.writeFileSync('../readme.md', readme);
+fs.writeFileSync(path.resolve(__dirname, '../readme.md'), readme);
+console.log(`[${new Date().toLocaleString()}] 封面文档已经生成，共生成 ${total} / ${components.length} 个。`)
+
